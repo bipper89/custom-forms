ed2e3b7552bd5dbdd775ba598eab11e314431efd
Alvaro Pavon <alvaro.apavon@gmail.com>
2023-12-06 12:47:37 -0600

    Implemented control value accesor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { InputComponent } from './components/input/input.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    InputComponent,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  protected fb = inject(FormBuilder);
+  protected loginForm: FormGroup = this.fb.group({
+    user: [''],
+    password: [''],
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, FormsModule],
+  template: `
+    <mat-form-field appearance="outline">
+      <mat-label>{{ label }}</mat-label>
+      <input
+        matInput
+        [placeholder]="placeholder"
+        [type]="type"
+        [ngModel]="value"
+        [disabled]="isDisabled"
+        (ngModelChange)="onChange($event)"
+      />
+    </mat-form-field>
+  `,
+  styles: `mat-form-field {width: 100%;}`,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() label = '';
+  @Input() placeholder = '';
+  @Input() type = 'text';
+
+  value: string = '';
+  isDisabled: boolean = false;
+  onChange = (_: any) => {};
+  onTouch = () => {};
+
+  writeValue(obj: any): void {
+    if (obj && obj !== '') {
+      this.value = obj;
+    }
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+}
